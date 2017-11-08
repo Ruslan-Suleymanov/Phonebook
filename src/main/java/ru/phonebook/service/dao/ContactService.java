91d382e9789d02f4d71037a84199c36c9fbd9d8b
Ruslan Suleymanov <mr.ruslan.suleymanov@gmail.com>
2017-11-08 23:12:56 +0300

    В процессе создания класс dao

---
+++ src/main/java/ru/phonebook/service/dao/ContactService.java
@@ -0,0 +1,24 @@
+package ru.phonebook.service.dao;
+
+import ru.phonebook.model.Contact;
+
+import java.util.List;
+
+public class ContactService {
+
+    public void save(Contact contact) {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public List<Contact> showAll() {
+
+        return null;
+
+    }
+
+
+}
