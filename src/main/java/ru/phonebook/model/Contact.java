955fe77205a63f91b3bee412dc6ee7079d0b2e7e
Ruslan Suleymanov <mr.ruslan.suleymanov@gmail.com>
2017-11-08 20:02:44 +0300

    Создали основную модель нашего приложения

---
+++ src/main/java/ru/phonebook/model/Contact.java
@@ -0,0 +1,85 @@
+package ru.phonebook.model;
+
+public class Contact {
+
+    private Integer id;
+    private String name;
+    private String surname;
+    private Integer phoneNumber;
+
+
+    public Contact() {
+    }
+
+    public Contact(Integer id, String name, String surname, Integer phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contact contact = (Contact) o;
+
+        if (id != null ? !id.equals(contact.id) : contact.id != null) return false;
+        if (name != null ? !name.equals(contact.name) : contact.name != null) return false;
+        if (surname != null ? !surname.equals(contact.surname) : contact.surname != null) return false;
+        return phoneNumber != null ? phoneNumber.equals(contact.phoneNumber) : contact.phoneNumber == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
