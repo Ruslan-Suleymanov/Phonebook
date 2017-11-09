e774925e42c19f244e3f993f5668f4008af15855
Ruslan Suleymanov <mr.ruslan.suleymanov@gmail.com>
2017-11-09 15:00:55 +0300

    Добавил конструктор без поля id

--- src/main/java/ru/phonebook/main/Main.java
+++ src/main/java/ru/phonebook/main/Main.java
@@ -7,11 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Contact test = new Contact(1, "Вася", "Пупкин", "89524561232");
+        Contact test = new Contact("Вася", "Пупкин", "89524561232");
 
         ContactService contactService = new ContactService();
         contactService.save(test);
-        contactService.save(new Contact(2, "Ваня", "Мупкин", "89456421232"));
+        contactService.save(new Contact("Ваня", "Мупкин", "89456421232"));
     }
 
 }
--- src/main/java/ru/phonebook/model/Contact.java
+++ src/main/java/ru/phonebook/model/Contact.java
@@ -18,6 +18,12 @@ public class Contact {
         this.phoneNumber = phoneNumber;
     }
 
+    public Contact(String name, String surname, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+
 
     public Integer getId() {
         return id;
